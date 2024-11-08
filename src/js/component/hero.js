14d143e547231ac13433d1029d468c41be18f4a6
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-11-08 15:55:30 +0000

    added a new Star Wars logo in Hero component

--- src/js/component/hero.js
+++ src/js/component/hero.js
@@ -2,12 +2,14 @@
 import React from "react";
 
 export const Hero = () => {
+
     return (
         <div className="px-4 py-0 my-0 text-center">
-            <img className="d-block mx-auto mb-4" src="https://www.elsoldezamora.com.mx/doble-via/hnjh5n-logo-star-wars/ALTERNATES/LANDSCAPE_1140/Logo%20Star%20Wars" style={{width:"75%"}}/>
+            <img className="d-block mx-auto mb-4" src="https://raw.githubusercontent.com/4GeeksAcademy/EliasMilano-Star-Wars-Blog/refs/heads/master/src/img/Logo-Star-Wars_3.jpg" style={{width:"100%"}}/>
             <div className="col-lg-6 mx-auto">
             </div>
         </div>
+        
     )
 }
 
