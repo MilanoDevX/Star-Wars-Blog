06a11ff3dfe5b9a4fe92c4e5fcfe8079c165bddd
MilanoTester <eliasmilano.tester@gmail.com>
2025-05-20 12:08:00 -0300

    fixed issue in hero image

--- src/js/component/hero.js
+++ src/js/component/hero.js
@@ -1,26 +1,32 @@
 
 import React, { useEffect } from "react";
 import { useLocation } from 'react-router-dom';
+import logoImage from "../../img/Logo-Star-Wars_3.jpg";
 
 export const Hero = () => {
 
-    const { hash } = useLocation();
+  const { hash } = useLocation();
 
-    useEffect(() => {
-      if (hash) {
-        const elemento = document.querySelector(hash);
-        if (elemento) {
-          elemento.scrollIntoView({ behavior: "smooth" });
-        }
+  useEffect(() => {
+    if (hash) {
+      const elemento = document.querySelector(hash);
+      if (elemento) {
+        elemento.scrollIntoView({ behavior: "smooth" });
       }
-    }, [hash]);
+    }
+  }, [hash]);
 
-    return (
-        <div id="hero" className="px-4 py-0 my-0 text-center">
-            <img className="d-block mx-auto mb-4" src="https://raw.githubusercontent.com/4GeeksAcademy/EliasMilano-Star-Wars-Blog/refs/heads/master/src/img/Logo-Star-Wars_3.jpg" style={{width:"100%"}}/>
-            <div className="col-lg-6 mx-auto">
-            </div>
-        </div>
-        
-    )
+  return (
+    <div id="hero" className="px-4 py-0 my-0 text-center">
+      <img
+        className="d-block mx-auto mb-4"
+        src={logoImage}
+        alt="Star Wars Logo"
+        style={{ width: "95%"}}
+      />
+      <div className="col-lg-6 mx-auto">
+      </div>
+
+    </div>
+  )
 }
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -7,6 +7,7 @@ import CardStarships from "../component/cardStarships";
 import { Context } from "../store/appContext"
 import { Hero } from "../component/hero";
 import { Link, useLocation, ScrollRestoration } from 'react-router-dom';
+import episodeImage from "../../img/Episode-3.jpg";
 
 export const Home = () => {
 
@@ -29,30 +30,30 @@ export const Home = () => {
 			<h1 id="databank" className="text-light pt-5 pb-0" >Star Wars Databank</h1>
 
 			<div className="container d-flex justify-content-center mb-1 gap-5" >
-				<Link to={{pathname:"/films"}} style={{ textDecoration: "none" }}>
+				<Link to={{ pathname: "/films" }} style={{ textDecoration: "none" }}>
 					<h4 className="text-light">Films</h4>
 				</Link>
 
-				<Link to={{pathname:"/characters"}} style={{ textDecoration: "none" }}>
+				<Link to={{ pathname: "/characters" }} style={{ textDecoration: "none" }}>
 					<h4 className="text-light">Characters</h4>
 				</Link>
 
-				<Link to={{pathname:"/planets"}} style={{ textDecoration: "none" }}>
+				<Link to={{ pathname: "/planets" }} style={{ textDecoration: "none" }}>
 					<h4 className="text-light">Planets</h4>
 				</Link>
 
-				<Link to={{pathname:"/starships"}} style={{ textDecoration: "none" }}>
+				<Link to={{ pathname: "/starships" }} style={{ textDecoration: "none" }}>
 					<h4 className="text-light">Starships</h4>
 				</Link>
 
-				<Link to={{pathname:"/vehicles"}} style={{ textDecoration: "none" }}>
+				<Link to={{ pathname: "/vehicles" }} style={{ textDecoration: "none" }}>
 					<h4 className="text-light">Vehicles</h4>
 				</Link>
-			</div>
-
-			<div className="d-flex justify-content-center">
-				<img src="https://raw.githubusercontent.com/4GeeksAcademy/EliasMilano-Star-Wars-Blog/refs/heads/master/src/img/Episode-3.jpg" 
-					style={{width:"70%"}}
+			</div>			<div className="d-flex justify-content-center">
+				<img
+					src={episodeImage}
+					alt="Star Wars Episode 3"
+					style={{ width: "70%" }}
 				/>
 			</div>
 
